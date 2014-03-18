80c53e7120a64e01622e9a81d0fc2a5dbbd5fda7
dv <wdv4758h@gmail.com>
2014-03-18 20:28:30 +0800

    remove unuse twig.php

--- twig.php
+++
@@ -1,8 +0,0 @@
-<?php
-//require_once 'Twig/Autoloader.php';
-require_once './vendor/autoload.php';
-//Twig_Autoloader::register();
-
-$loader = new Twig_Loader_Filesystem('template/');
-$twig = new Twig_Environment($loader);
-?>
