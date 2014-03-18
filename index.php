8d5892a6fd3a1b304b58b5bcdc79524bf93d99ce
dv <wdv4758h@gmail.com>
2014-03-18 20:27:52 +0800

    remove redundant code

--- index.php
+++ index.php
@@ -5,11 +5,4 @@ if(!isset($_SESSION['id']))
     header('location: login');
 else
     header('location: main');
-
-
-/*
-if (array_key_exists('view', $_GET))
-    include_once('view/'.$_GET['view'].'.php');
-*/
-
 ?>
