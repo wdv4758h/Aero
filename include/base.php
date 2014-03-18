1fd3ddc7b9d8987504338432e2dae40f20044296
dv <wdv4758h@gmail.com>
2014-03-18 20:21:02 +0800

    add include/base.php

---
+++ include/base.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+
+// Composer autoload
+require_once './vendor/autoload.php';
+
+// twig
+function render($template, $parameter){
+    $loader = new Twig_Loader_Filesystem('./template');
+    $twig = new Twig_Environment($loader);
+
+    //$parameter['session'] = $_SESSION;
+    echo $twig->render($template, $parameter);
+    return;
+}
+
+?>
