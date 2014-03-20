b2cfc1a788176ede203ba6b75f8b01707d9c9fcf
dv <wdv4758h@gmail.com>
2014-03-20 19:13:13 +0800

    redirect unauthorized api connection to login page

--- api/planes.php
+++ api/planes.php
@@ -1,6 +1,14 @@
 <?php
 require_once('../include/db.php');
 
+session_start();
+
+// Redirect to Login Page for unauthorized connection...
+if(!isset($_SESSION['id'])) {
+    header('location: ../login');
+    exit();
+}
+
 $aero = new Aero();
 $aero -> sql = 'SELECT * FROM flights';
 $aero -> execute();
