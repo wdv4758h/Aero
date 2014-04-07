6afcada3cbeb0d44e0eb79b8e145dcf0c383398d
dv <wdv4758h@gmail.com>
2014-04-07 21:23:24 +0800

    add isEmpty method to Object
    hide comparison list if it is empty

--- static/js/controller.js
+++ static/js/controller.js
@@ -136,3 +136,11 @@ app.controller('airportsList', function($scope, $http){
 
     getAirportsList();
 });
+
+Object.prototype.isEmpty = function() {
+    for(var key in this) {
+        if(this.hasOwnProperty(key))
+            return false;
+    }
+    return true;
+}
