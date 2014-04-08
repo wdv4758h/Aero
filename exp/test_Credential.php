0bbbd7a377b18fe26749160bb590115ab796ec86
Pu-Hsuan Wu <hwupu.cs01@nctu.edu.tw>
2014-04-08 15:09:09 +0800

    Add checkCredntial in base.php

---
+++ exp/test_Credential.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once '../include/base.php';
+
+checkCredential();
+
+?>
--- include/base.php
+++ include/base.php
@@ -1,5 +1,32 @@
 <?php
 
+class Credential {
+    const isAdmin = 1;
+}
+
+function checkCredential($level = 0) {
+    if(!isset($_SESSION['id'])) {
+        //echo "not login";
+        header('location: /login');
+        exit();
+    }
+    if($level==Credential::isAdmin && !$_SESSION['is_admin']) {
+        //echo "not admin";
+        header('location: /');
+        exit();
+    }
+    require_once 'aero.php';
+    $user = new User;
+    $user -> get($_SESSION['id']);
+    if($user->id == null) {
+        //echo "not in databas";
+        header('location: /login');
+        exit();
+    }
+    $_SESSION['is_admin'] = $user -> is_admin;
+}
+
+
 // start session if it is not started before
 if (session_id() == "")
     session_start();
