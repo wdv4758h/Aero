37196bb794f42955c672ee508ace2c93fb897b79
dv <wdv4758h@gmail.com>
2014-05-22 21:05:51 +0800

    add Country delete

---
+++ country/delete.php
@@ -0,0 +1,34 @@
+<?php
+require_once '../include/base.php';
+checkCredential(Credential::isAdmin);
+
+require_once('../include/aero.php');
+$country = new Country();
+
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    if(!isset($_POST['id'])) {
+        echo "Can not be empty";
+    }
+
+    $country -> delete($_POST['id']);
+
+    header('location: /country/');
+
+} else if($_SERVER['REQUEST_METHOD'] === 'GET') {
+
+    if(!isset($_GET['id'])) {
+        header('location: /country/');
+    }
+
+    $country -> get($_GET['id']);
+
+    render('country_delete.html', array('country'=>$country));
+
+} else {
+
+    header('location: /country/');
+
+}
+
+?>
