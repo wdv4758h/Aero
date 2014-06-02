2c9ca45c2ab80aa3964aa1446eefb515b52ac21f
dv <wdv4758h@gmail.com>
2014-06-02 22:53:37 +0800

    redirect admin login to tickets

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ session_start();
 require_once 'include/base.php';
 
 if ( isset($_SESSION['id']) && $_SESSION['is_admin'])
-    header('location: /flights/');
+    header('location: /tickets/');
 else
     render('homepage.html', compact('_SESSION'));
 ?>
