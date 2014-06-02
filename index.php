62faabbbb04635ebcb305ef8a8087ed43c803df4
Pu-Hsuan Wu <hwupu.cs01@nctu.edu.tw>
2014-06-02 23:06:22 +0800

    Work on routes after logged in

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 session_start();
 require_once 'include/base.php';
 
-if ( isset($_SESSION['id']) && $_SESSION['is_admin'])
+if ( isset($_SESSION['id']))
     header('location: /tickets/');
 else
     render('homepage.html', compact('_SESSION'));
--- tickets/index.php
+++ tickets/index.php
@@ -1,7 +1,9 @@
 <?php
 require_once('../include/base.php');
 
-// Render...
-render('ticket.html', compact('_SESSION'));
+if ( isset($_SESSION['id']) && $_SESSION['is_admin'])
+    render('ticket.html', compact('_SESSION'));
+else
+    render('tickets_list_ng@newAERO.html', compact('_SESSION'));
 
 ?>
